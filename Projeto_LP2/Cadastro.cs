bb1582505c8b78b6069680764710bb064e239383
renanzudo <renan.designer@gmail.com>
2018-09-22 12:49:17 -0300

    xxx

---
+++ Projeto_LP2/Cadastro.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_LP2
+{
+    public partial class Cadastro : Form
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cadastro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_UsuarioCadastro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Projeto_LP2/Controle e Visualização com Tipo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_LP2
+{
+    public partial class Controle_e_Visualização_com_Tipo : Form
+    {
+        public Controle_e_Visualização_com_Tipo()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_AddControlView_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstViewControleTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
