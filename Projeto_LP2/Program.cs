19aa56ef66ec13899e5491b46b3b257d6cd41aa4
renanzudo <renan.designer@gmail.com>
2018-09-22 13:25:25 -0300

    ggg

--- Projeto_LP2/Program.cs
+++ Projeto_LP2/Program.cs
@@ -18,7 +18,7 @@ namespace Projeto_LP2
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Cadastro());
 
         }
     }
